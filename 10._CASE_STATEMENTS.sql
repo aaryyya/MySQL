72225546cd0cd89c24f9f2ebe36a47d8b1490788
aaryyya <aryampatil2005@gmail.com>
2025-08-22 11:55:43 +0530

    Updates in Case Statements

--- 10._CASE_STATEMENTS.sql
+++ 10._CASE_STATEMENTS.sql
@@ -23,9 +23,24 @@ from employee_demographics;
 select salary, first_name,last_name,
 case 
 	when salary < 50000 then salary+(salary*0.05)
+    #or u can use then salary*1.05 
+    #or u can use then salary*1.07 
     when salary>50000 then salary+(salary*0.07)
 end as new_salary 
 from employee_salary;
+
+select first_name,last_name,salary,
+case
+	when salary<50000 then salary*1.05
+	when salary>50000 then salary*1.07
+    else 'No Hike'
+end as new_salary,
+case 
+	when dept_id=6 then salary*0.10
+    else 'No Hike'
+end as Bonus
+from employee_salary;
+
 
 
 
